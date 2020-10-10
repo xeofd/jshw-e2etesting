934bf122923fd07c50490f51153307b6305a4b94
ben <benfd@outlook.com>
2020-10-10 13:25:58 +0100

    13:25, 10/10/20 - Done e2e tests upto second last one will do final test later

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,63 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should display correctly when operators are used', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#operator_equals').click();
+
+    cy.get('.display').should('contain', '3');
+  });
+
+  it('should allow for multiple operator chains', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+
+    cy.get('.display').should('contain', '6');
+  });
+
+  it('should display whole numbers', () => {
+    cy.get('#number2').click();
+
+    cy.get('.display').should('contain', '2');
+  });
+
+  it('should display negative numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+
+    cy.get('.display').should('contain', '-1');
+  });
+
+  it('should display decimal numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+
+    cy.get('.display').should('contain', '0.6');
+  });
+
+  it('should display large numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+
+    cy.get('.display').should('contain', '2000');
+  });
+
+  
+
 })
